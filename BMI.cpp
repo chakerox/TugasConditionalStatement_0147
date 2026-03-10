2f71a0d2c815b4c1d292e519dee8714c4f84e1dc
chakerox <chakerox@gmail.com>
2026-03-10 20:36:45 +0700

    prosedur input data

--- BMI.cpp
+++ BMI.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 
+//prosedur memasukan data
+
+void inputData(float &berat, float &tinggi)
+{
+
+    cout << "Masukkan berat badan (kg): ";
+
+    cin >> berat;
+
+    cout << "Masukkan tinggi badan (meter): ";
+
+    cin >> tinggi;
+
+}
